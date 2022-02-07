a413fa1f0931bd57d8645911d2b74e06e1ee260c
joeledwardson <joel.edwardson1@gmail.com>
2022-02-07 18:30:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rete React Typescript Example
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Project source is migrated from [Rete.js React render sandbox](https://codesandbox.io/s/t899c?file=/public/index.html) and converted to use typescript (not strict.)
+
+Intended to serve as a base for rete.js projects using React and Typescript.
+
+![image-20220207171946229](https://imgur.com/ZTzcOOQ.png)
---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState, RefObject, createRef } from "react";
+import ReactDOM from "react-dom";
+import { createEditor } from "./rete";
+
+function Editor() {
+  const divRef = createRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (divRef.current) {
+      createEditor(divRef.current);
+    } else {
+      throw new Error("div ref not initialized!");
+    }
+  });
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      ref={divRef}
+    />
+  );
+}
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Editor />, rootElement);
